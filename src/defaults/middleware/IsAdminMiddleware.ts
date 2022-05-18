d6fdfc20e9cf0cfb1e2868c0ad25d072458b60f4
Zechariah <zechariahtan144@gmail.com>
2022-05-19 00:22:58 +0800

    🐛 fix is admin middleware

--- src/defaults/middleware/IsAdminMiddleware.ts
+++ src/defaults/middleware/IsAdminMiddleware.ts
@@ -1,16 +1,13 @@
 import { GuildMember } from "discord.js"
 
-import BaseEntry from "../../bases/BaseEntry"
-import BaseGuildCache from "../../bases/BaseGuildCache"
-import ResponseBuilder from "../../builders/ResponseBuilder"
-import { SlashHelper, SlashMiddleware } from "../../interactions/slash"
+import { BaseEntry, BaseGuildCache, CommandHelper, CommandMiddleware, ResponseBuilder } from "../.."
 
 export default class IsAdminMiddleware<
 	E extends BaseEntry,
 	GC extends BaseGuildCache<E, GC>
-> extends SlashMiddleware<E, GC> {
-	handler(helper: SlashHelper<E, GC>): boolean | Promise<boolean> {
-		const member = helper.interaction.member as GuildMember
+> extends CommandMiddleware<any, E, GC> {
+	handler(helper: CommandHelper<any, E, GC>): boolean | Promise<boolean> {
+		const member = (helper.interaction || helper.message)!.member as GuildMember
 		if (!member.permissions.has("ADMINISTRATOR") && member.id !== process.env.DISCORD__DEV_ID) {
 			helper.respond(ResponseBuilder.bad("Only administrators can use this slash command"))
 			return false
