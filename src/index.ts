d675d8aceb6117aaf1137f865923cb479676d311
Zechariah Tan <zechariahtan144@gmail.com>
2022-05-19 11:04:48 +0800

    🐛 export types instead of declaring

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { MessageActionRow, MessageEmbed } from "discord.js"
+
 import NovaBot from "./NovaBot"
 
 export { default as BaseBotCache, iBaseBotCache } from "./bases/BaseBotCache"
@@ -11,7 +13,12 @@ export { default as SlashBuilder } from "./builders/SlashBuilder"
 export { default as IsAdminMiddleware } from "./defaults/middleware/IsAdminMiddleware"
 
 export { default as BaseButton, ButtonMiddleware, ButtonHelper } from "./interactions/button"
-export { default as BaseCommand, CommandMiddleware, CommandHelper, CommandType } from "./interactions/command"
+export {
+	default as BaseCommand,
+	CommandMiddleware,
+	CommandHelper,
+	CommandType
+} from "./interactions/command"
 export { default as BaseEvent, EventMiddleware } from "./interactions/event"
 export {
 	default as BaseSelectMenu,
@@ -27,6 +34,44 @@ export { default as LogManager } from "./utils/LogManager"
 export { default as SlashCommandDeployer } from "./utils/SlashCommandDeployer"
 
 export * from "./NovaBot"
-export * from "./@types/types"
+
+export type CommandPayload = {
+	embeds: MessageEmbed[]
+	components: MessageActionRow[]
+}
+
+export interface iSlashData {
+	name: string
+	description: string
+	options?: (iSlashDefaultOption | iSlashStringOption | iSlashNumberOption)[]
+}
+
+export interface iSlashOption {
+	name: string
+	description: string
+	requirements: string
+	required: boolean
+	default?: string
+}
+
+export interface iSlashDefaultOption extends iSlashOption {
+	type: "boolean" | "user" | "role" | "channel" | "mentionable"
+}
+
+export interface iSlashStringOption extends iSlashOption {
+	type: "string"
+	choices?: {
+		name: string
+		value: string
+	}[]
+}
+
+export interface iSlashNumberOption extends iSlashOption {
+	type: "number"
+	choices?: {
+		name: string
+		value: number
+	}[]
+}
 
 export default NovaBot
