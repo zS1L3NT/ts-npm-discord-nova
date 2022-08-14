1d65004678d6f7d569c78d50b66035918c4b39ea
Zechariah <zechariahtan144@gmail.com>
2022-08-14 23:38:46 +0800

    ✨ more in-depth permissioned commands

--- src/utils/EventSetupHelper.ts
+++ src/utils/EventSetupHelper.ts
@@ -30,7 +30,6 @@ export default class EventSetupHelper<
 			if (message.author.bot) return
 			if (!message.guild) return
 			const cache = await this.botCache.getGuildCache(message.guild!)
-			if (!cache.isAdministrator) return
 
 			await this.onMessage(cache, message)
 		})
@@ -38,11 +37,6 @@ export default class EventSetupHelper<
 		this.botCache.bot.on("interactionCreate", async interaction => {
 			if (!interaction.guild) return
 			const cache = await this.botCache.getGuildCache(interaction.guild!)
-			if (
-				!cache.isAdministrator &&
-				!(interaction.isCommand() && interaction.commandName === "help")
-			)
-				return
 
 			if (interaction.isCommand()) await this.onSlashInteraction(cache, interaction)
 			if (interaction.isButton()) await this.onButtonInteraction(cache, interaction)
@@ -56,30 +50,39 @@ export default class EventSetupHelper<
 
 		for (const [name, commandFile] of this.fsh.commandFiles) {
 			const helper = new CommandHelper(name, CommandType.Message, cache, undefined, message)
-
 			if (commandFile.only === CommandType.Slash) continue
 			if (!commandFile.condition(helper)) continue
 
 			logger.discord(`Opening MessageCommand(${name}) for User(${message.author.tag})`)
 
-			try {
-				helper.params = commandFile.converter(helper) ?? {}
-				await message.channel
-					.sendTyping()
-					.catch(err => logger.warn("Failed to send typing after message command", err))
+			if (cache.isAdministrator) {
+				try {
+					helper.params = commandFile.converter(helper) ?? {}
+					await message.channel
+						.sendTyping()
+						.catch(err =>
+							logger.warn("Failed to send typing after message command", err)
+						)
 
-				let broke = false
-				for (const middleware of commandFile.middleware) {
-					if (await middleware.handler(helper)) continue
-					broke = true
-					break
+					let broke = false
+					for (const middleware of commandFile.middleware) {
+						if (await middleware.handler(helper)) continue
+						broke = true
+						break
+					}
+
+					if (!broke) await commandFile.execute(helper)
+				} catch (err) {
+					logger.error("Error executing message command", err)
+					helper.respond(
+						ResponseBuilder.bad("There was an error while executing this command!")
+					)
 				}
-
-				if (!broke) await commandFile.execute(helper)
-			} catch (err) {
-				logger.error("Error executing message command", err)
+			} else {
 				helper.respond(
-					ResponseBuilder.bad("There was an error while executing this command!")
+					ResponseBuilder.bad(
+						"Administrator Permissions required for message commands to work"
+					)
 				)
 			}
 
@@ -89,19 +92,18 @@ export default class EventSetupHelper<
 	}
 
 	private async onSlashInteraction(cache: GC, interaction: CommandInteraction) {
-		const commandFile = this.fsh.commandFiles.get(interaction.commandName)
-		if (!commandFile) return
-
-		logger.discord(
-			`Opening SlashInteraction(${interaction.commandName}) for User(${interaction.user.tag})`
-		)
-
 		const helper = new CommandHelper(
 			interaction.commandName,
 			CommandType.Slash,
 			cache,
 			interaction,
 			undefined
+		)
+		const commandFile = this.fsh.commandFiles.get(interaction.commandName)
+		if (!commandFile) return
+
+		logger.discord(
+			`Opening SlashInteraction(${interaction.commandName}) for User(${interaction.user.tag})`
 		)
 
 		if (commandFile.defer) {
@@ -110,20 +112,28 @@ export default class EventSetupHelper<
 				.catch(err => logger.error("Failed to defer slash interaction", err))
 		}
 
-		try {
-			let broke = false
-			for (const middleware of commandFile.middleware) {
-				if (await middleware.handler(helper)) continue
-				broke = true
-				break
-			}
+		if (cache.isAdministrator || interaction.commandName === "help") {
+			try {
+				let broke = false
+				for (const middleware of commandFile.middleware) {
+					if (await middleware.handler(helper)) continue
+					broke = true
+					break
+				}
 
-			if (!broke) {
-				await commandFile.execute(helper)
+				if (!broke) {
+					await commandFile.execute(helper)
+				}
+			} catch (err) {
+				logger.error("Error executing slash interaction", err)
+				helper.respond(
+					ResponseBuilder.bad("There was an error while executing this command!")
+				)
 			}
-		} catch (err) {
-			logger.error("Error executing slash interaction", err)
-			helper.respond(ResponseBuilder.bad("There was an error while executing this command!"))
+		} else {
+			helper.respond(
+				ResponseBuilder.bad("Administrator Permissions required for slash commands to work")
+			)
 		}
 
 		logger.discord(
@@ -132,8 +142,10 @@ export default class EventSetupHelper<
 	}
 
 	private async onButtonInteraction(cache: GC, interaction: ButtonInteraction) {
+		const helper = new ButtonHelper(cache, interaction)
 		const buttonFile = this.fsh.buttonFiles.get(interaction.customId)
 		if (!buttonFile) return
+
 		logger.discord(
 			`Opening ButtonInteraction(${interaction.customId}) for User(${interaction.user.tag})`
 		)
@@ -144,28 +156,38 @@ export default class EventSetupHelper<
 				.catch(err => logger.error("Failed to defer button interaction", err))
 		}
 
-		const helper = new ButtonHelper(cache, interaction)
-		try {
-			let broke = false
-			for (const middleware of buttonFile.middleware) {
-				if (await middleware.handler(helper)) continue
-				broke = true
-				break
-			}
+		if (cache.isAdministrator) {
+			try {
+				let broke = false
+				for (const middleware of buttonFile.middleware) {
+					if (await middleware.handler(helper)) continue
+					broke = true
+					break
+				}
 
-			if (!broke) await buttonFile.execute(helper)
-		} catch (err) {
-			logger.error("Error executing button interaction", err)
-			helper.respond(ResponseBuilder.bad("There was an error while executing this command!"))
+				if (!broke) await buttonFile.execute(helper)
+			} catch (err) {
+				logger.error("Error executing button interaction", err)
+				helper.respond(
+					ResponseBuilder.bad("There was an error while executing this command!")
+				)
+			}
+		} else {
+			helper.respond(
+				ResponseBuilder.bad("Administrator Permissions required for buttons to work")
+			)
 		}
+
 		logger.discord(
 			`Closing ButtonInteraction(${interaction.customId}) for User(${interaction.user.tag})`
 		)
 	}
 
 	private async onSelectMenuInteraction(cache: GC, interaction: SelectMenuInteraction) {
+		const helper = new SelectMenuHelper(cache, interaction)
 		const selectMenuFile = this.fsh.selectMenuFiles.get(interaction.customId)
 		if (!selectMenuFile) return
+
 		logger.discord(
 			`Opening SelectMenuInteraction(${interaction.customId}) for User(${interaction.user.tag})`
 		)
@@ -176,28 +198,38 @@ export default class EventSetupHelper<
 				.catch(err => logger.error("Failed to defer select menu interaction", err))
 		}
 
-		const helper = new SelectMenuHelper(cache, interaction)
-		try {
-			let broke = false
-			for (const middleware of selectMenuFile.middleware) {
-				if (await middleware.handler(helper)) continue
-				broke = true
-				break
-			}
+		if (cache.isAdministrator) {
+			try {
+				let broke = false
+				for (const middleware of selectMenuFile.middleware) {
+					if (await middleware.handler(helper)) continue
+					broke = true
+					break
+				}
 
-			if (!broke) await selectMenuFile.execute(helper)
-		} catch (err) {
-			logger.error("Error executing select menu command", err)
-			helper.respond(ResponseBuilder.bad("There was an error while executing this command!"))
+				if (!broke) await selectMenuFile.execute(helper)
+			} catch (err) {
+				logger.error("Error executing select menu command", err)
+				helper.respond(
+					ResponseBuilder.bad("There was an error while executing this command!")
+				)
+			}
+		} else {
+			helper.respond(
+				ResponseBuilder.bad("Administrator Permissions required for select menus to work")
+			)
 		}
+
 		logger.discord(
 			`Closing SelectMenuInteraction(${interaction.customId}) for User(${interaction.user.tag})`
 		)
 	}
 
 	private async onModalInteraction(cache: GC, interaction: ModalSubmitInteraction) {
+		const helper = new ModalHelper(cache, interaction)
 		const modalFile = this.fsh.modalFiles.get(interaction.customId)
 		if (!modalFile) return
+
 		logger.discord(
 			`Opening ModalInteraction(${interaction.customId}) for User(${interaction.user.tag})`
 		)
@@ -208,19 +240,30 @@ export default class EventSetupHelper<
 				.catch(err => logger.error("Failed to defer select menu interaction", err))
 		}
 
-		const helper = new ModalHelper(cache, interaction)
-		try {
-			let broke = false
-			for (const middleware of modalFile.middleware) {
-				if (await middleware.handler(helper)) continue
-				broke = true
-				break
-			}
+		if (cache.isAdministrator) {
+			try {
+				let broke = false
+				for (const middleware of modalFile.middleware) {
+					if (await middleware.handler(helper)) continue
+					broke = true
+					break
+				}
 
-			if (!broke) await modalFile.execute(helper)
-		} catch (err) {
-			logger.error("Error executing select menu command", err)
-			helper.respond(ResponseBuilder.bad("There was an error while executing this command!"))
+				if (!broke) await modalFile.execute(helper)
+			} catch (err) {
+				logger.error("Error executing select menu command", err)
+				helper.respond(
+					ResponseBuilder.bad("There was an error while executing this command!")
+				)
+			}
+		} else {
+			helper.respond(
+				ResponseBuilder.bad("Administrator Permissions required for modals to work")
+			)
 		}
+
+		logger.discord(
+			`Closing ModalInteraction(${interaction.customId}) for User(${interaction.user.tag})`
+		)
 	}
 }
